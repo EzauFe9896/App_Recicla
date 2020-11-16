c1ee36957f92651ba4f82db6fef4f8f5729e4f3d
Ezau José <69482703+EzauFe9896@users.noreply.github.com>
2020-11-16 16:38:02 -0700

    Update create.blade.php
    Se añadio el mensaje error.

--- resources/views/registro/create.blade.php
+++ resources/views/registro/create.blade.php
@@ -32,6 +32,20 @@
                 <h2><strong>Formulario de registro para ser voluntario</strong></h2>
             </div>
 
+
+{{-- Muestra el mensaje error si no se registra correctamente --}}
+@if (session('error'))
+<div class="col-sm-12">
+    <div class="alert  alert-danger alert-dismissible fade show" role="alert">
+        {{ session('error') }}
+            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+    </div>
+</div>
+@endif
+{{-- Fin del mensaje --}}
+
             
 <form name="formulario" method="post" action="{{url('registro/add')}}" class="needs-validation" novalidate>
     @csrf
